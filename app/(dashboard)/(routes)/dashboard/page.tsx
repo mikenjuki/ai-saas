5ed811f76dc3a89ce471eace39c5ffb5fffa1d66
Mike Njuki <mikenjuki@gmail.com>
2023-07-28 23:03:32 +0300

    initial setup

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+const DashboardPage = () => {
+  return (
+    <>
+      <h1 className="">Dashboard Page (protected)</h1>
+
+      <Button>Sign Up</Button>
+    </>
+  );
+};
+
+export default DashboardPage;
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LandingPage = () => {
+  return <div>LandingPage (public)</div>;
+};
+
+export default LandingPage;
