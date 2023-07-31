ad2fcef5c81cf0429c9c42b21cbcec80bd7fe7ae
Mike Njuki <mikenjuki@gmail.com>
2023-08-01 00:09:52 +0300

    Created image route and modified it's schema

---
+++ app/(dashboard)/(routes)/image/page.tsx
@@ -0,0 +1,175 @@
+"use client";
+
+import * as z from "zod";
+import { ImageIcon } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { ChatCompletionRequestMessage } from "openai";
+import axios from "axios";
+import ReactMarkdown from "react-markdown";
+
+import { formSchema } from "./constants";
+
+import Heading from "@/components/Heading";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import Empty from "@/components/Empty";
+import Loader from "@/components/Loader";
+import { cn } from "@/lib/utils";
+import UserAvatar from "@/components/user-avatar";
+import BotAvatar from "@/components/bot-avatar";
+
+const ImagePage = () => {
+  const router = useRouter();
+  const [messages, setMessages] = useState<ChatCompletionRequestMessage[]>([]);
+
+  //initialize form from shad which uses zod & react hook form
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: "",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      // Create a 'userMessage' object with role "user" and content from the 'values.prompt' input property
+      const userMessage: ChatCompletionRequestMessage = {
+        role: "user",
+        content: values.prompt,
+      };
+      // Create a new array 'newMessages' by copying all the elements from 'messages' array and appending the 'userMessage'.
+      // To keep context of the chat
+      const newMessages = [...messages, userMessage];
+
+      // Send a POST request to the "/api/code" endpoint with the 'newMessages' array as the payload.
+      const response = await axios.post("/api/code", {
+        messages: newMessages,
+      });
+
+      // Update the 'messages' state by appending both 'userMessage' and the 'response.data' from the API to the current state.
+      // To be used to display the chats
+      setMessages((current) => [...current, userMessage, response.data]);
+
+      form.reset();
+    } catch (error: any) {
+      // open pro modal: TODO
+      console.log(error);
+    } finally {
+      // Regardless of whether there was an error or not, refresh the router  to update the UI
+      router.refresh();
+    }
+  };
+
+  return (
+    <div>
+      <Heading
+        title={"Image Generation"}
+        description={"Turn your prompt into an image"}
+        icon={ImageIcon}
+        iconColor="text-pink-700"
+        bgColor="bg-pink-700/10"
+      />
+
+      <div className="px-4 lg:px-8">
+        <div>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="
+                rounded-lg
+                border
+                w-full
+                p-4
+                px-3
+                md:px-6
+                focus-within:shadow-sm
+                grid
+                grid-cols-12
+                gap-2
+              "
+            >
+              <FormField
+                name="prompt"
+                render={({ field }) => (
+                  <FormItem className="col-span-12 lg:col-span-10">
+                    <FormControl className="m-0 p-0">
+                      <Input
+                        className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                        disabled={isLoading}
+                        placeholder="React jsx boilerplate code"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+
+              <Button
+                className="col-span-12 lg:col-span-2 w-full"
+                type="submit"
+                disabled={isLoading}
+                size="icon"
+              >
+                Generate
+              </Button>
+            </form>
+          </Form>
+        </div>
+
+        <div className="space-y-4 mt-4">
+          {isLoading && (
+            <div className="p-8 rounded-lg w-full flex items-center justify-center bg-muted">
+              <Loader />
+            </div>
+          )}
+
+          {messages.length === 0 && !isLoading && (
+            <div>
+              <Empty label={"Conversation is empty"} />
+            </div>
+          )}
+
+          <div className="flex flex-col-reverse gap-y-4">
+            {messages.map((message) => (
+              <div
+                key={message.content}
+                className={cn(
+                  "p-8 w-full flex items-start gap-x-8 rounded-lg",
+                  message.role === "user"
+                    ? "bg-white border border-black/10"
+                    : "bg-muted"
+                )}
+              >
+                {message.role === "user" ? <UserAvatar /> : <BotAvatar />}
+
+                <ReactMarkdown
+                  components={{
+                    pre: ({ node, ...props }) => (
+                      <div className="overflow-auto w-full my-2 bg-black/10 p-2 rounded-lg">
+                        <pre {...props} />
+                      </div>
+                    ),
+                    code: ({ node, ...props }) => (
+                      <code className="bg-black/10 rounded-lg p-1" {...props} />
+                    ),
+                  }}
+                  className="text-sm overflow-hidden leading-7"
+                >
+                  {message.content || ""}
+                </ReactMarkdown>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ImagePage;
